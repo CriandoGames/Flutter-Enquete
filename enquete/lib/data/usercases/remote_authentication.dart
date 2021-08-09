655ee841abfde67c2443434d41b96e708d5fe214
Carlos Castro <carloscastrogames@gmail.com>
2021-08-09 15:07:36 -0300

    ♻️ :  move api details from domain class to data class

--- enquete/lib/data/usercases/remote_authentication.dart
+++ enquete/lib/data/usercases/remote_authentication.dart
@@ -1,3 +1,5 @@
+import 'package:enquete/domain/entities/entities.dart';
+
 import '../../domain/usercases/usercases.dart';
 
 import '../http/http.dart';
@@ -8,6 +10,24 @@ class RemoteAuthentication {
 
   const RemoteAuthentication({required this.httpClient, required this.url});
   Future<void> auth(AutenticationParams params) async {
-    await httpClient.request(url: url, method: 'post', body: params.toJson());
+    await httpClient.request(
+        url: url,
+        method: 'post',
+        body: RemoteAutenticationParams.fromDomain(params).toJson());
   }
+}
+
+class RemoteAutenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAutenticationParams({
+    required this.email,
+    required this.password,
+  });
+
+  factory RemoteAutenticationParams.fromDomain(AutenticationParams entity) =>
+      RemoteAutenticationParams(email: entity.email, password: entity.password);
+
+  Map toJson() => {'email': email, 'password': password};
 }
--- enquete/lib/domain/usercases/authentication.dart
+++ enquete/lib/domain/usercases/authentication.dart
@@ -12,6 +12,4 @@ class AutenticationParams {
     required this.email,
     required this.password,
   });
-
-  Map toJson() => {'email': email, 'password': password};
 }
