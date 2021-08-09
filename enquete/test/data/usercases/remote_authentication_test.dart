40c932b271234d8d986086b6b3dcb04399edda5b
Carlos Castro <carloscastrogames@gmail.com>
2021-08-09 14:39:58 -0300

    ✅ Test: Ensure RemoteAuthentication calls HttpsClient with correct body

--- enquete/test/data/usercases/remote_authentication_test.dart
+++ enquete/test/data/usercases/remote_authentication_test.dart
@@ -1,22 +1,23 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:enquete/domain/usercases/usercases.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
   final String url;
 
   const RemoteAuthentication({required this.httpClient, required this.url});
-  Future<void> auth() async {
-    await httpClient.request(url: url, method: 'post');
+  Future<void> auth(AutenticationParams params) async {
+    final body = {'email': params.email, 'password': params.password};
+    await httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({
-    required String url,
-    required String method,
-  });
+  Future<void>? request(
+      {required String url, required String method, Map? body});
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
@@ -27,14 +28,19 @@ late RemoteAuthentication sut;
 
 void main() {
   setUp(() {
-    sut = RemoteAuthentication(httpClient: httpClient, url: url);
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
   });
 
   test('shuld call httpClient with correct value', () async {
-    await sut.auth();
+    final params = AutenticationParams(
+        email: faker.internet.email(), password: faker.internet.password());
+    await sut.auth(params);
 
-    verify(httpClient.request(url: url, method: 'post'));
+    verify(httpClient.request(
+        url: url,
+        method: 'post',
+        body: {'email': params.email, 'password': params.password}));
   });
 }
