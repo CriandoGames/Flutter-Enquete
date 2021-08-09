aefb4ba727a34295f1c36ee2199fd9668363c474
Carlos Castro <carloscastrogames@gmail.com>
2021-08-09 14:54:33 -0300

    ♻️

--- enquete/lib/domain/usercases/authentication.dart
+++ enquete/lib/domain/usercases/authentication.dart
@@ -12,4 +12,6 @@ class AutenticationParams {
     required this.email,
     required this.password,
   });
+
+  Map toJson() => {'email': email, 'password': password};
 }
--- enquete/test/data/usercases/remote_authentication_test.dart
+++ enquete/test/data/usercases/remote_authentication_test.dart
@@ -2,23 +2,14 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+
+import 'package:enquete/data/http/http.dart';
+
+import 'package:enquete/data/usercases/usercases.dart';
 import 'package:enquete/domain/usercases/usercases.dart';
 
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
 
-  const RemoteAuthentication({required this.httpClient, required this.url});
-  Future<void> auth(AutenticationParams params) async {
-    final body = {'email': params.email, 'password': params.password};
-    await httpClient.request(url: url, method: 'post', body: body);
-  }
-}
 
-abstract class HttpClient {
-  Future<void>? request(
-      {required String url, required String method, Map? body});
-}
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
