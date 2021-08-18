7481e4f8b4098a2a1b6d6dccc0aa79f2326c9cb8
Carlos Castro <carloscastrogames@gmail.com>
2021-08-18 14:29:41 -0300

    ✅ Test: should disable button if form is invalid

--- enquete/test/ui/pages/login_pages_test.dart
+++ enquete/test/ui/pages/login_pages_test.dart
@@ -135,4 +135,14 @@ void main() {
     final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
     expect(button.onPressed, isNotNull);
   });
+
+    testWidgets('should disable button if form is invalid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+    isFormValidController.add(false);
+    await tester.pump();
+
+    final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
+    expect(button.onPressed, null);
+  });
 }
