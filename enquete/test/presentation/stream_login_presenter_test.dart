f9442c66d382b1c7751e0aebdc373bab0a304ad8
Carlos Castro <carloscastrogames@gmail.com>
2021-08-23 11:29:30 -0300

    ✅ Test: refactor method ValidationSpy

--- enquete/test/presentation/stream_login_presenter_test.dart
+++ enquete/test/presentation/stream_login_presenter_test.dart
@@ -9,7 +9,7 @@ abstract class Validation {
 }
 
 class LoginState {
-   late String? emailError;
+  late String? emailError;
 }
 
 class StreamLoginPresenter {
@@ -34,10 +34,19 @@ void main() {
   late ValidationSpy validation;
   late StreamLoginPresenter sut;
   late String email;
+
+  PostExpectation mockValidationCall({String? field}) =>
+      when(validation.validation(field: field ?? 'field', email: 'value'));
+
+  PostExpectation? mockValidation({String? field, String? value}) {
+    mockValidationCall(field: field).thenReturn(value);
+  }
+
   setUp(() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    mockValidation();
   });
 
   test('should call validation with correct email', () {
@@ -46,8 +55,7 @@ void main() {
   });
 
   test('should emit  error if validation fails', () {
-    when(validation.validation(field: 'field', email: 'value'))
-        .thenReturn('error');
+    mockValidation(value: 'error');
 
     expectLater(sut.emailErrorStream, emits('error'));
 
